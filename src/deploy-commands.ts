a6a7f7725b32af507593edff41fe044fe3bc55ce
Thorben Below <thorbenbelow@gmail.com>
2023-04-01 15:11:49 +0200

    add deploy script

---
+++ src/deploy-commands.ts
@@ -0,0 +1,24 @@
+import {REST, Routes} from 'discord.js';
+import ping from './commands/ping';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const rest = new REST({version: '10'}).setToken(process.env.DISCORD_TOKEN);
+
+const commands = [
+  ping,
+].map((c) => c.command.toJSON());
+
+try {
+  console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+  await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      {body: commands},
+  );
+
+  console.log(`Successfully reloaded application (/) commands.`);
+} catch (error) {
+  console.error(error);
+}
