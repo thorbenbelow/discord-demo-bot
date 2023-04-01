6798637850c469a959d4eb5b03a5c643e3e4b5b1
Thorben Below <thorbenbelow@gmail.com>
2023-04-01 14:21:55 +0200

    init

---
+++ src/client.ts
@@ -0,0 +1,12 @@
+import {Client, Events, GatewayIntentBits} from 'discord.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({intents: GatewayIntentBits.Guilds});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as user ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
